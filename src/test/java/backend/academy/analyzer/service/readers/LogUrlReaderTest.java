a178891f1ee667077ef516bd3d13378ceeb1e365
moksem <maksim.buynov.00@mail.ru>
2024-10-25 22:13:37 +0300

    fix: исправлены тесты

--- src/test/java/backend/academy/analyzer/service/readers/LogUrlReaderTest.java
+++ src/test/java/backend/academy/analyzer/service/readers/LogUrlReaderTest.java
@@ -11,8 +11,6 @@ public class LogUrlReaderTest {
     private static final String CORRECT_URL =
         "https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs";
 
-    private static final String INCORRECT_URL = "https://example.com";
-
     private static final String INVALID_URL = "a:";
 
     private final LogReader reader = new LogUrlReader();
@@ -20,12 +18,6 @@ public class LogUrlReaderTest {
     @Test
     public void checkReadAllLogsSuccess() {
         Stream<String> stream = reader.read(CORRECT_URL);
-        assertTrue(stream.findAny().isPresent());
-    }
-
-    @Test
-    public void checkReadAllLogsDenied() {
-        Stream<String> stream = reader.read(INCORRECT_URL);
         assertTrue(stream.findAny().isPresent());
     }
 
