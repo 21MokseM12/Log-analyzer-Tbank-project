266bb90d9fe8ea1c5b90e1e3d58b94da73bc1d1b
moksem <maksim.buynov.00@mail.ru>
2024-10-25 20:02:21 +0300

    feature: реализован класс валидации параметра --to и --path

--- src/main/java/backend/academy/analyzer/validators/PathParameterValidator.java
+++ src/main/java/backend/academy/analyzer/validators/PathParameterValidator.java
@@ -1,9 +1,24 @@
 package backend.academy.analyzer.validators;
 
-public class PathParameterValidator implements ParameterValidator{
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathParameterValidator implements ParameterValidator {
+
     @Override
     public boolean isValid(String paramBody) {
-        //todo
-        return false;
+        try {
+            URI uri = new URI(paramBody);
+            if (uri.getScheme() != null && (uri.getScheme().equals("http") || uri.getScheme().equals("https"))) {
+                return true;
+            }
+            throw new URISyntaxException(uri.toString(), "Path is not URI");
+        } catch (URISyntaxException _) {
+            Path path = Paths.get(paramBody);
+            return Files.exists(path);
+        }
     }
 }
