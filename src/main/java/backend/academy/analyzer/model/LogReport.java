cb385c3be895d9529a8ae495e83ef4e8eda55dfa
moksem <maksim.buynov.00@mail.ru>
2024-10-26 20:34:46 +0300

    fix: изменены методы доступа к полям

--- src/main/java/backend/academy/analyzer/model/LogReport.java
+++ src/main/java/backend/academy/analyzer/model/LogReport.java
@@ -3,24 +3,19 @@ package backend.academy.analyzer.model;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
-import javax.annotation.Nullable;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
 @Getter
-@Setter
 @EqualsAndHashCode
 public class LogReport {
 
     private List<String> sourceName;
 
-    @Nullable
     private LocalDateTime from;
 
-    @Nullable
     private LocalDateTime to;
 
     private long logCount;
@@ -29,13 +24,7 @@ public class LogReport {
 
     private double percentServerResponse;
 
-    Map<String, Integer> popularResources;
+    private Map<String, Integer> popularResources;
 
-    Map<String, Integer> popularStatusCodes;
-
-    public LogReport(List<String> sourceName, @Nullable LocalDateTime from, @Nullable LocalDateTime to) {
-        this.sourceName = sourceName;
-        this.from = from;
-        this.to = to;
-    }
+    private Map<Integer, Integer> popularStatusCodes;
 }
