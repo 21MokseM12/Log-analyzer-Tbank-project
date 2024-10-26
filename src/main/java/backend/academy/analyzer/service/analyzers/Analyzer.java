d36607ddc16a385678d3d94f071e5f831abceb69
moksem <maksim.buynov.00@mail.ru>
2024-10-26 20:34:09 +0300

    fix: добавлены аргументы в интерфейс анализатора логов

--- src/main/java/backend/academy/analyzer/service/analyzers/Analyzer.java
+++ src/main/java/backend/academy/analyzer/service/analyzers/Analyzer.java
@@ -2,8 +2,10 @@ package backend.academy.analyzer.service.analyzers;
 
 import backend.academy.analyzer.model.Log;
 import backend.academy.analyzer.model.LogReport;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.Stream;
 
 public interface Analyzer {
-    LogReport analyze(Stream<Log> logs);
+    LogReport analyze(Stream<Log> logs, List<String> sources, LocalDateTime from, LocalDateTime to);
 }
