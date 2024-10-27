29314ec8b5a1693298a2a30ab21e1a96b3d15283
moksem <maksim.buynov.00@mail.ru>
2024-10-27 17:03:06 +0300

    test: добавлены тесты для парсера ресурсов

---
+++ src/test/java/backend/academy/analyzer/parsers/ResourceParserImplTest.java
@@ -0,0 +1,52 @@
+package backend.academy.analyzer.parsers;
+
+import backend.academy.analyzer.enums.PathType;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.NoSuchElementException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ResourceParserImplTest {
+
+    private static final String LOCAL_PATH =
+        "./src/test/resources/logs/logs.txt";
+
+    private static final String LOCAL_PATH_PATTERN_FOR_SINGLE_FILE =
+        "./src/test/resources/logs/2024*.txt";
+
+    private static final String LOCAL_PATH_PATTERN_FOR_MULTIPLE_FILE =
+        "./src/test/resources/logs/logs*.txt";
+
+    private static final String INVALID_LOCAL_PATH = "./hello/world*";
+
+    private static final String URL_PATH =
+        "https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs";
+
+    private final ResourceParser parser = new ResourceParserImpl();
+
+    @Test
+    public void checkValidParseResourceNameByLocalPath() {
+        assertEquals(List.of("logs.txt"), parser.parsePathResourceName(PathType.LOCAL, LOCAL_PATH));
+    }
+
+    @Test
+    public void checkValidParseResourceNameByLocalPathPatternSingleFile() {
+        assertEquals(List.of("2024example.txt"), parser.parsePathResourceName(PathType.LOCAL, LOCAL_PATH_PATTERN_FOR_SINGLE_FILE));
+    }
+
+    @Test
+    public void checkValidParseResourceNameByLocalPathPatternMultipleFile() {
+        assertEquals(List.of("logs.txt", "logs2.txt"), parser.parsePathResourceName(PathType.LOCAL, LOCAL_PATH_PATTERN_FOR_MULTIPLE_FILE));
+    }
+
+    @Test
+    public void checkThrowsExceptionIfLocalFileNotFound() {
+        assertThrows(NoSuchElementException.class, () -> parser.parsePathResourceName(PathType.LOCAL, INVALID_LOCAL_PATH));
+    }
+
+    @Test
+    public void checkValidParseResourceNameByUrl() {
+        assertEquals(List.of("nginx_logs"), parser.parsePathResourceName(PathType.URL, URL_PATH));
+    }
+}
