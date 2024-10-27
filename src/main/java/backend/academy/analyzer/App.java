fbe89a593af06d9bb4e620afd88a036ee9fc2004
moksem <maksim.buynov.00@mail.ru>
2024-10-27 23:07:43 +0300

    feature: добавлена проверка того, что параметр --path точно был введен; добавлена логика получения отчета о логах и их печать

--- src/main/java/backend/academy/analyzer/App.java
+++ src/main/java/backend/academy/analyzer/App.java
@@ -2,49 +2,58 @@ package backend.academy.analyzer;
 
 import backend.academy.analyzer.enums.ParameterType;
 import backend.academy.analyzer.enums.PrintFormat;
-import backend.academy.analyzer.managers.ValidatorManager;
+import backend.academy.analyzer.factories.PrinterFactory;
+import backend.academy.analyzer.factories.ValidatorFactory;
+import backend.academy.analyzer.model.LogReport;
+import backend.academy.analyzer.service.creators.LogReportCreator;
+import backend.academy.analyzer.service.creators.LogReportCreatorImpl;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class App {
 
-//    private static final String DEFAULT_FORMAT_VALUE = "markdown";
+    private static final PrintFormat DEFAULT_FORMAT_VALUE = PrintFormat.MARKDOWN;
 
     private static final Map<ParameterType, String> argumentMap = new HashMap<>();
 
-    private static final ValidatorManager validatorManager = new ValidatorManager();
+    private static final ValidatorFactory VALIDATOR_FACTORY = new ValidatorFactory();
 
-//    private static final Analyzer analyzer = new LoggerAnalyzer();
-//
-//    private static final PrinterManager printerManager = new PrinterManager();
+    private static final LogReportCreator LOG_REPORT_CREATOR = new LogReportCreatorImpl();
 
-    private App() {}
+    private static final PrinterFactory printerFactory = new PrinterFactory();
+
+    private App() {
+    }
 
     public static void main(String[] args) {
         if (args.length < 2 || args.length % 2 != 0) {
             throw new IllegalArgumentException("Параметры некорректны");
         }
 
-        for (int i = 0; i < args.length; i+=2) {
+        for (int i = 0; i < args.length; i += 2) {
             for (ParameterType type : ParameterType.values()) {
                 if (type.toString().equals(args[i])) {
-                    argumentMap.put(type, args[i+1]);
+                    argumentMap.put(type, args[i + 1]);
                 }
             }
         }
 
+        if (!argumentMap.containsKey(ParameterType.PATH)) {
+            throw new IllegalArgumentException("Параметр %s не найден".formatted(ParameterType.PATH.toString()));
+        }
+
         for (Map.Entry<ParameterType, String> entry : argumentMap.entrySet()) {
-            if (!validatorManager.get(entry.getKey()).isValid(entry.getValue())) {
+            if (!VALIDATOR_FACTORY.get(entry.getKey()).isValid(entry.getValue())) {
                 throw new IllegalArgumentException("Тело параметра %s некорректно".formatted(entry.getKey()));
             }
         }
 
-//        Statistic statistic = analyzer.analyze(argumentMap);
-//
-//        printerManager.get(
-//            PrintFormat.valueOf(
-//                argumentMap.getOrDefault("--format", DEFAULT_FORMAT_VALUE).toUpperCase()
-//            )
-//        ).print();
+        LogReport report = LOG_REPORT_CREATOR.create(argumentMap);
+
+        printerFactory.get(
+            PrintFormat.valueOf(
+                argumentMap.getOrDefault(ParameterType.FORMAT, DEFAULT_FORMAT_VALUE.toString()).toUpperCase()
+            )
+        ).print(report);
     }
 }
