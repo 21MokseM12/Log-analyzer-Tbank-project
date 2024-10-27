29fc7e97216ba36293e0e8242edde7eec83b3001
moksem <maksim.buynov.00@mail.ru>
2024-10-27 20:46:18 +0300

    test: добавлены тесты и подправлена логика валидации параметра --from

--- src/main/java/backend/academy/analyzer/validators/FromParameterValidator.java
+++ src/main/java/backend/academy/analyzer/validators/FromParameterValidator.java
@@ -1,9 +1,9 @@
 package backend.academy.analyzer.validators;
 
-import lombok.extern.log4j.Log4j2;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class FromParameterValidator implements ParameterValidator {
@@ -11,8 +11,8 @@ public class FromParameterValidator implements ParameterValidator {
     @Override
     public boolean isValid(String paramBody) {
         try {
-            DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE_TIME;
-            LocalDateTime.parse(paramBody, timeFormatter);
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+            LocalDate.parse(paramBody, timeFormatter);
             return true;
         } catch (DateTimeParseException e) {
             log.error("Переданный формат времени --from не валиден", e);
---
+++ src/test/java/backend/academy/analyzer/validators/FromParameterValidatorTest.java
@@ -0,0 +1,43 @@
+package backend.academy.analyzer.validators;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FromParameterValidatorTest {
+
+    private static Stream<Arguments> validTime() {
+        return Stream.of(
+            Arguments.of("2024-08-31"),
+            Arguments.of("2017-10-30"),
+            Arguments.of("2003-02-01")
+        );
+    }
+
+    private static Stream<Arguments> invalidTime() {
+        return Stream.of(
+            Arguments.of("31-08-2024"),
+            Arguments.of("08-31-2024"),
+            Arguments.of("2024-08-35"),
+            Arguments.of("2024-16-31"),
+            Arguments.of("456-08-31")
+        );
+    }
+
+    private final ParameterValidator validator = new FromParameterValidator();
+
+    @ParameterizedTest
+    @MethodSource("validTime")
+    public void checkValidTime(String time) {
+        assertTrue(validator.isValid(time));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidTime")
+    public void checkInvalidTime(String time) {
+        assertFalse(validator.isValid(time));
+    }
+}
