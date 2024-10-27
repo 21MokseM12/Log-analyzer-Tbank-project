eede2df6ea406c433440a58f9dd5ce799e4552cb
moksem <maksim.buynov.00@mail.ru>
2024-10-27 20:44:01 +0300

    test: добавлены тесты и подправлена логика валидации параметра --to

--- src/main/java/backend/academy/analyzer/validators/ToParameterValidator.java
+++ src/main/java/backend/academy/analyzer/validators/ToParameterValidator.java
@@ -1,17 +1,19 @@
 package backend.academy.analyzer.validators;
 
 import lombok.extern.log4j.Log4j2;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 @Log4j2
 public class ToParameterValidator implements ParameterValidator {
+
     @Override
     public boolean isValid(String paramBody) {
         try {
-            DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE_TIME;
-            LocalDateTime.parse(paramBody, timeFormatter);
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+            LocalDate.parse(paramBody, timeFormatter);
             return true;
         } catch (DateTimeParseException e) {
             log.error("Переданный формат времени --to не валиден", e);
