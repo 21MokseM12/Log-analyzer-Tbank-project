3853adb3dc8d305ebb5cc5be1ecf8562440ca348
moksem <maksim.buynov.00@mail.ru>
2024-10-27 16:38:05 +0300

    fix: метод определения типа пути перемещен в enum PathType

--- src/main/java/backend/academy/analyzer/enums/PathType.java
+++ src/main/java/backend/academy/analyzer/enums/PathType.java
@@ -1,5 +1,29 @@
 package backend.academy.analyzer.enums;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+
 public enum PathType {
-    URL, LOCAL
+    URL, LOCAL;
+
+    public static PathType identify(String path) {
+        try {
+            URI uri = new URI(path);
+            if (uri.getScheme() != null && (uri.getScheme().equals("http") || uri.getScheme().equals("https"))) {
+                return PathType.URL;
+            }
+            throw new URISyntaxException(uri.toString(), "Path is not URI");
+        } catch (URISyntaxException _) {
+            Path localPath = Paths.get(path);
+            if (Files.exists(localPath)) {
+                return PathType.LOCAL;
+            } else {
+                throw new NoSuchElementException("Указанный путь не найден");
+            }
+        }
+    }
 }
