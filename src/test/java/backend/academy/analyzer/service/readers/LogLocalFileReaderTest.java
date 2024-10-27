4b97a702c209ad305130bfe70cb70e4b655d1400
moksem <maksim.buynov.00@mail.ru>
2024-10-27 21:06:02 +0300

    test: добавлены тесты к классу чтения файлов

--- src/test/java/backend/academy/analyzer/service/readers/LogLocalFileReaderTest.java
+++ src/test/java/backend/academy/analyzer/service/readers/LogLocalFileReaderTest.java
@@ -8,8 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LogLocalFileReaderTest {
 
-    private static final String CORRECT_PATH =
+    private static final String CORRECT_PATH_TO_SINGLE_FILE =
         "./src/test/resources/logs/logs.txt";
+
+    private static final String CORRECT_PATH_TO_MULTIPLE_FILES =
+        "./src/test/resources/logs/2024*.txt";
 
     private static final String INVALID_PATH = "./src/test/resources/log/log.txt";
 
@@ -17,7 +20,13 @@ public class LogLocalFileReaderTest {
 
     @Test
     public void checkReadAllLogsSuccess() {
-        Stream<String> stream = reader.read(CORRECT_PATH);
+        Stream<String> stream = reader.read(CORRECT_PATH_TO_SINGLE_FILE);
+        assertTrue(stream.findAny().isPresent());
+    }
+
+    @Test
+    public void checkReadMultipleFilesByWildCardSuccess() {
+        Stream<String> stream = reader.read(CORRECT_PATH_TO_MULTIPLE_FILES);
         assertTrue(stream.findAny().isPresent());
     }
 
