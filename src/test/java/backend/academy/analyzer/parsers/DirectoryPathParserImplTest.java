f0077a71c818ec7582ae32a75eee912b047af44b
moksem <maksim.buynov.00@mail.ru>
2024-10-27 17:26:48 +0300

    test: добавлены тесты к классу парсинга шаблонов локальных путей

---
+++ src/test/java/backend/academy/analyzer/parsers/DirectoryPathParserImplTest.java
@@ -0,0 +1,30 @@
+package backend.academy.analyzer.parsers;
+
+import org.junit.jupiter.api.Test;
+import java.nio.file.Path;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DirectoryPathParserImplTest {
+
+    private static final String PATH_WITHOUT_WILDCARDS = "./src/test/resources/logs/logs.txt";
+
+    private static final String PATH_WITH_WILDCARDS = "./src/test/resources/logs/logs*";
+
+    @Test
+    public void checkGetParentDirectoryByPathWithoutWildcards() {
+        DirectoryPathParser parser = new DirectoryPathParserImpl(PATH_WITHOUT_WILDCARDS);
+        assertEquals(Path.of(PATH_WITHOUT_WILDCARDS), parser.getParentDirectory());
+    }
+
+    @Test
+    public void checkGetParentDirectoryByPathWithWildcards() {
+        DirectoryPathParser parser = new DirectoryPathParserImpl(PATH_WITH_WILDCARDS);
+        assertEquals(Path.of(PATH_WITH_WILDCARDS.substring(0, PATH_WITH_WILDCARDS.lastIndexOf("/"))), parser.getParentDirectory());
+    }
+
+    @Test
+    public void checkGetPathPatternByPathWithWildcards() {
+        DirectoryPathParser parser = new DirectoryPathParserImpl(PATH_WITH_WILDCARDS);
+        assertEquals(PATH_WITH_WILDCARDS.substring(PATH_WITH_WILDCARDS.lastIndexOf("/") + 1), parser.getPathPattern());
+    }
+}
