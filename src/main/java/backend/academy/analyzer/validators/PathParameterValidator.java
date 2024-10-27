fae7d87599407f8fa487c450524931d883518950
moksem <maksim.buynov.00@mail.ru>
2024-10-27 20:26:40 +0300

    fix: изменена логика валидации параметра --path

--- src/main/java/backend/academy/analyzer/validators/PathParameterValidator.java
+++ src/main/java/backend/academy/analyzer/validators/PathParameterValidator.java
@@ -2,23 +2,65 @@ package backend.academy.analyzer.validators;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 public class PathParameterValidator implements ParameterValidator {
 
     @Override
     public boolean isValid(String paramBody) {
+        if (paramBody.isEmpty()) return false;
+        if (isValidUrl(paramBody)) {
+            return true;
+        } else {
+            return isValidFilePath(paramBody);
+        }
+    }
+
+    private boolean isValidUrl(String url) {
         try {
-            URI uri = new URI(paramBody);
-            if (uri.getScheme() != null && (uri.getScheme().equals("http") || uri.getScheme().equals("https"))) {
-                return true;
-            }
-            throw new URISyntaxException(uri.toString(), "Path is not URI");
+            URI uri = new URI(url);
+            return uri.getScheme() != null && (uri.getScheme().equals("http") || uri.getScheme().equals("https"));
         } catch (URISyntaxException _) {
-            Path path = Paths.get(paramBody);
-            return Files.exists(path);
+            return false;
+        }
+    }
+
+    private boolean isValidFilePath(String path) {
+        if (isValidLocalPath(path)) {
+            return true;
+        } else {
+            return isValidPathPattern(path);
+        }
+    }
+
+    private boolean isValidPathPattern(String input) {
+
+        // Регулярное выражение для проверки наличия символов "*" или "?"
+        String regex = ".*[?*].*";
+        boolean first = Pattern.matches(regex, input);
+
+        try {
+            // Пытаемся создать PathMatcher в формате glob для строки
+            PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + input);
+            return first;
+        } catch (IllegalArgumentException e) {
+            // Если выбрасывается исключение, строка не является шаблоном
+            return false;
+        }
+    }
+
+    private boolean isValidLocalPath(String input) {
+        try {
+            Path path = Paths.get(input);
+            return Files.exists(path) && Files.isRegularFile(path);
+        } catch (InvalidPathException _) {
+            return false;
         }
     }
 }
