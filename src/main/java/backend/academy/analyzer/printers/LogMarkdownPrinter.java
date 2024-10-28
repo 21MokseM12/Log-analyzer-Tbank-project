fca86028e39b14b2739e4708862b16d29f308f6a
moksem <maksim.buynov.00@mail.ru>
2024-10-28 15:43:48 +0300

    refactor: добавлены комментарии

--- src/main/java/backend/academy/analyzer/printers/LogMarkdownPrinter.java
+++ src/main/java/backend/academy/analyzer/printers/LogMarkdownPrinter.java
@@ -48,44 +48,57 @@ public class LogMarkdownPrinter implements LogPrinter {
         String from = report.from().equals(LocalDateTime.MIN) ? "-" : report.from().toString();
         String to = report.to().equals(LocalDateTime.MAX) ? "-" : report.to().toString();
 
+        // добавление оглавления таблицы
         formattedReport.append(HEAD_DESIGNATION).append(ReportTopic.GENERAL_INFO_LABEL).append(DOUBLE_NEW_LINE);
+        // добавление первой строки таблицы с типами данных внутри нее
         formattedReport.append(TABLE_DELIMITER).append(TABULATION_DOUBLE_SPACE).append(ReportTopic.METRIC)
             .append(TABULATION_DOUBLE_SPACE).append(TABLE_DELIMITER)
             .append(TABULATION_DOUBLE_SPACE).append(ReportTopic.MEANING)
             .append(DOUBLE_TABULATION).append(TABLE_DELIMITER).append("\n");
+        // добавление разделение первой строки и остальных
         formattedReport.append(TABLE_DELIMITER).append(TABLE_HEAD_DELIMITER)
             .append(TABLE_DELIMITER).append(TABLE_HEAD_DELIMITER).append(TABLE_DELIMITER).append("\n");
+        //  добавление второй строки таблицы с данными
         formattedReport.append(TABLE_DELIMITER)
             .append(TABULATION_DOUBLE_SPACE).append(ReportTopic.INPUT_RESOURCES_NAME).append(TABULATION_DOUBLE_SPACE)
             .append(TABLE_DELIMITER)
             .append(report.sourceName().toString(), 1, report.sourceName().toString().length() - 1)
             .append(TRIPLE_TABULATION).append(TABLE_DELIMITER).append("\n");
+        //  добавление третьей строки таблицы с данными
         formattedReport.append(TABLE_DELIMITER).append("  ").append(ReportTopic.BEGIN_DATE)
             .append(" ").append(TABLE_DELIMITER).append(DOUBLE_TABULATION_DOUBLE_SPACE).append(from)
             .append(TRIPLE_TABULATION).append(TABLE_DELIMITER).append("\n");
+        //  добавление четвертой строки таблицы с данными
         formattedReport.append(TABLE_DELIMITER).append("  ").append(ReportTopic.END_DATE)
             .append("  ").append(TABLE_DELIMITER).append(DOUBLE_TABULATION_DOUBLE_SPACE).append(to)
             .append(TRIPLE_TABULATION).append(TABLE_DELIMITER).append("\n");
+        //  добавление пятой строки таблицы с данными
         formattedReport.append(TABLE_DELIMITER).append(ReportTopic.LOG_COUNT)
             .append(TABLE_DELIMITER).append(TABULATION_DOUBLE_SPACE).append(report.logCount())
             .append(DOUBLE_TABULATION).append(TABLE_DELIMITER).append("\n");
+        //  добавление шестой строки таблицы с данными
         formattedReport.append(TABLE_DELIMITER).append(ReportTopic.AVG_SERVER_RESPONSE)
             .append(TABLE_DELIMITER).append(TABULATION_DOUBLE_SPACE).append(report.avgServerResponse())
             .append(DOUBLE_TABULATION).append(TABLE_DELIMITER).append("\n");
+        //  добавление седьмой строки таблицы с данными
         formattedReport.append(TABLE_DELIMITER).append(ReportTopic.PERCENT_SERVER_RESPONSE)
             .append(TABLE_DELIMITER).append(TABULATION_DOUBLE_SPACE).append(report.percentServerResponse())
             .append(DOUBLE_TABULATION).append(TABLE_DELIMITER).append(DOUBLE_NEW_LINE);
     }
 
     private void addResourceTable(StringBuilder formattedReport, LogReport report) {
+        // добавление оглавления таблицы
         formattedReport.append(HEAD_DESIGNATION)
             .append(ReportTopic.GENERAL_MOST_POPULAR_RESOURCES_LABEL).append(DOUBLE_NEW_LINE);
+        // добавление первой строки таблицы с типами данных внутри нее
         formattedReport.append(TABLE_DELIMITER).append(TABULATION_DOUBLE_SPACE).append(ReportTopic.RESOURCE_NAME)
             .append(TABULATION_DOUBLE_SPACE).append(TABLE_DELIMITER)
             .append(TABULATION_DOUBLE_SPACE).append(ReportTopic.COUNT)
             .append("\t").append(TABLE_DELIMITER).append("\n");
+        // добавление разделение первой строки и остальных
         formattedReport.append(TABLE_DELIMITER).append(TABLE_HEAD_DELIMITER)
             .append(TABLE_DELIMITER).append(TABLE_HEAD_DELIMITER).append(TABLE_DELIMITER).append("\n");
+        // добавление всех данных из мапы в таблицу
         for (Map.Entry<String, Integer> entry : report.popularResources().entrySet()) {
             formattedReport.append(TABLE_DELIMITER)
                 .append(TABULATION_DOUBLE_SPACE).append(entry.getKey()).append(TABULATION_DOUBLE_SPACE)
@@ -97,16 +110,20 @@ public class LogMarkdownPrinter implements LogPrinter {
     }
 
     private void addStatusCodeTable(StringBuilder formattedReport, LogReport report) {
+        // добавление оглавления таблицы
         formattedReport.append(HEAD_DESIGNATION)
             .append(ReportTopic.GENERAL_MOST_POPULAR_STATUS_CODES_LABEL).append(DOUBLE_NEW_LINE);
+        // добавление первой строки таблицы с типами данных внутри нее
         formattedReport.append(TABLE_DELIMITER).append(TABULATION_DOUBLE_SPACE).append(ReportTopic.STATUS_CODE_VALUE)
             .append(DOUBLE_TABULATION_DOUBLE_SPACE).append(TABLE_DELIMITER)
             .append(DOUBLE_TABULATION_DOUBLE_SPACE).append(ReportTopic.STATUS_CODE_NAME)
             .append(TABULATION_FOUR_SPACE).append(TABLE_DELIMITER).append("\t ").append(ReportTopic.COUNT)
             .append(TABULATION_DOUBLE_SPACE).append(TABLE_DELIMITER).append("\n");
+        // добавление разделение первой строки и остальных
         formattedReport.append(TABLE_DELIMITER).append(TABLE_HEAD_DELIMITER)
             .append(TABLE_DELIMITER).append(TABLE_HEAD_DELIMITER)
             .append(TABLE_DELIMITER).append(TABLE_HEAD_DELIMITER).append(TABLE_DELIMITER).append("\n");
+        // добавление всех данных из мапы в таблицу
         for (Map.Entry<Integer, Integer> entry : report.popularStatusCodes().entrySet()) {
             formattedReport.append(TABLE_DELIMITER)
                 .append(TABULATION_FOUR_SPACE).append(entry.getKey()).append(DOUBLE_TABULATION_DOUBLE_SPACE)
