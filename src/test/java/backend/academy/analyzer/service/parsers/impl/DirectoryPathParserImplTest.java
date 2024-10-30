bc2928ce297bb060e8f6f1e379165de27a2b89f3
moksem <maksim.buynov.00@mail.ru>
2024-10-30 23:05:06 +0300

    test: добавлены тесты для парсера шаблона локального пути

--- src/test/java/backend/academy/analyzer/service/parsers/impl/DirectoryPathParserImplTest.java
+++ src/test/java/backend/academy/analyzer/service/parsers/impl/DirectoryPathParserImplTest.java
@@ -1,31 +1,49 @@
 package backend.academy.analyzer.service.parsers.impl;
 
 import backend.academy.analyzer.service.parsers.interfaces.DirectoryPathParser;
-import org.junit.jupiter.api.Test;
 import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DirectoryPathParserImplTest {
 
-    private static final String PATH_WITHOUT_WILDCARDS = "./src/test/resources/logs/logs.txt";
-
-    private static final String PATH_WITH_WILDCARDS = "./src/test/resources/logs/logs*";
-
-    @Test
-    public void checkGetParentDirectoryByPathWithoutWildcards() {
-        DirectoryPathParser parser = new DirectoryPathParserImpl(PATH_WITHOUT_WILDCARDS);
-        assertEquals(Path.of(PATH_WITHOUT_WILDCARDS), parser.getParentDirectory());
+    private static Stream<Arguments> validParentDirectories() {
+        return Stream.of(
+            Arguments.of("./src/test/resources/logs/logs.txt", Path.of("./src/test/resources/logs/logs.txt")),
+            Arguments.of("./src/test/resources/logs/logs*", Path.of("./src/test/resources/logs/")),
+            Arguments.of("./src/test/resources/**/logs*", Path.of("./src/test/resources/")),
+            Arguments.of("./src/test/?esources/**/logs*", Path.of("./src/test/"))
+        );
     }
 
-    @Test
-    public void checkGetParentDirectoryByPathWithWildcards() {
-        DirectoryPathParser parser = new DirectoryPathParserImpl(PATH_WITH_WILDCARDS);
-        assertEquals(Path.of(PATH_WITH_WILDCARDS.substring(0, PATH_WITH_WILDCARDS.lastIndexOf("/"))), parser.getParentDirectory());
+    private static Stream<Arguments> validFileNamePatterns() {
+        return Stream.of(
+            Arguments.of("./src/test/resources/logs/logs*", "logs*"),
+            Arguments.of("./src/test/resources/**/logs*", "logs*"),
+            Arguments.of("./src/test/?esources/**/*.txt", "*.txt")
+        );
     }
 
-    @Test
-    public void checkGetPathPatternByPathWithWildcards() {
-        DirectoryPathParser parser = new DirectoryPathParserImpl(PATH_WITH_WILDCARDS);
-        assertEquals(PATH_WITH_WILDCARDS.substring(PATH_WITH_WILDCARDS.lastIndexOf("/") + 1), parser.getPathPattern());
+    @ParameterizedTest
+    @MethodSource("validParentDirectories")
+    public void checkGetParentDirectory(String path, Path expected) {
+        DirectoryPathParser parser = new DirectoryPathParserImpl(path);
+        assertEquals(expected, parser.getParentDirectory());
     }
+
+    @ParameterizedTest
+    @MethodSource("validFileNamePatterns")
+    public void checkGetFileNamePattern(String path, String expected) {
+        DirectoryPathParser parser = new DirectoryPathParserImpl(path);
+        assertEquals(expected, parser.getFileNamePattern());
+    }
+
+//    @Test
+//    public void checkGetPathPatternByFileNameWithWildcards() {
+//        DirectoryPathParser parser = new DirectoryPathParserImpl(PATH_WITH_WILDCARDS);
+//        assertEquals(PATH_WITH_WILDCARDS.substring(PATH_WITH_WILDCARDS.lastIndexOf("/") + 1), parser.getFileNamePattern());
+//    }
 }
