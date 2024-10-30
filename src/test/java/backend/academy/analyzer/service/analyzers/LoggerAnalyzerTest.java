db99e2c2d1fd78d2b41fb0bbfaef5dc057e07c2c
moksem <maksim.buynov.00@mail.ru>
2024-10-30 17:57:08 +0300

    test: добавлены тесты для классов анализатора логов и парсера логов

--- src/test/java/backend/academy/analyzer/service/analyzers/LoggerAnalyzerTest.java
+++ src/test/java/backend/academy/analyzer/service/analyzers/LoggerAnalyzerTest.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoggerAnalyzerTest {
@@ -26,12 +25,16 @@ public class LoggerAnalyzerTest {
 
     @Test
     public void checkAnalyzeAllLogsSuccess() {
+        Map<String, Integer> resources = Map.of("product_1", 7, "product_2", 6);
+        Map<Integer, Integer> statusCodes = Map.of(304, 8, 404, 4, 200, 1);
+        Map<String, Integer> clientIps = Map.of("93.180.71.3", 2, "46.4.66.76", 2, "80.91.33.133", 2);
+        Map<String, Integer> userAgents = Map.of("Debian APT-HTTP/1.3", 12, "Go 1.1 package http", 1);
         LogReport firstResult = new LogReport(List.of("sources"), LocalDateTime.MIN, LocalDateTime.MAX,
             13, 138, 340,
-            Map.of("product_1", 7, "product_2", 6), Map.of(304, 8, 404, 4, 200, 1));
+            resources, statusCodes, clientIps, userAgents);
         LogReport secondResult = new LogReport(List.of("sources"), LocalDateTime.MIN, LocalDateTime.MAX,
             13, 138, 324,
-            Map.of("product_1", 7, "product_2", 6), Map.of(304, 8, 404, 4, 200, 1));
+            resources, statusCodes, clientIps, userAgents);
         Stream<Log> logs = logParser.parseToLog(reader.read(PATH), LocalDateTime.MIN, LocalDateTime.MAX);
         LogReport report = analyzer.analyze(logs, List.of("sources"), LocalDateTime.MIN, LocalDateTime.MAX);
         assertTrue(report.equals(firstResult) || report.equals(secondResult));
--- src/test/java/backend/academy/analyzer/service/parsers/impl/LogParserImplTest.java
+++ src/test/java/backend/academy/analyzer/service/parsers/impl/LogParserImplTest.java
@@ -1,10 +1,16 @@
 package backend.academy.analyzer.service.parsers.impl;
 
+import backend.academy.analyzer.model.Log;
 import backend.academy.analyzer.service.parsers.interfaces.LogParser;
 import backend.academy.analyzer.service.readers.LogLocalFileReader;
 import backend.academy.analyzer.service.readers.LogReader;
-import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,6 +20,16 @@ public class LogParserImplTest {
     private static final String PATH = "./src/test/resources/logs/logs.txt";
 
     private static final String REQUEST_SAMPLE = "GET /downloads/product_1 HTTP/1.1";
+
+    private static Stream<Arguments> validUserAgentPart() {
+        return Stream.of(
+            Arguments.of("\"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0\"", "Mozilla/5.0"),
+            Arguments.of("\"Debian APT-HTTP/1.3 (1.0.1ubuntu2)\"", "Debian APT-HTTP/1.3"),
+            Arguments.of("\"Chef Client/11.12.2 (ruby-1.9.3-p484; ohai-7.0.2; x86_64-linux; +http://opscode.com)\"", "Chef Client/11.12.2"),
+            Arguments.of("\"urlgrabber/3.10 yum/3.4.3\"", "urlgrabber/3.10 yum/3.4.3"),
+            Arguments.of("\"-\"", "-")
+        );
+    }
 
     private final LogParser parser = new LogParserImpl();
 
@@ -43,7 +59,7 @@ public class LogParserImplTest {
     public void checkParseExactlyOneLog() {
         assertTrue(parser.parseToLog(reader.read(PATH),
                 LocalDateTime.of(2015, 5, 17, 8, 5, 45),
-                LocalDateTime.of(2015, 5, 17, 10, 5, 45)
+                LocalDateTime.of(2015, 5, 17, 8, 5, 45)
             )
             .findAny()
             .isPresent());
@@ -60,7 +76,25 @@ public class LogParserImplTest {
     }
 
     @Test
+    public void checkCorrectLogConverting() {
+        List<Log> logs = parser.parseToLog(reader.read(PATH),
+            LocalDateTime.of(2015, 5, 17, 8, 5, 45),
+            LocalDateTime.of(2015, 5, 17, 8, 5, 45))
+            .toList();
+        List<Log> result = List.of(new Log("46.4.66.76", "-",
+            LocalDateTime.of(2015, 5, 17, 8, 5, 45), "\"GET /downloads/product_1 HTTP/1.1\"",
+            404, 318, "\"-\"", "\"Debian APT-HTTP/1.3 (1.0.1ubuntu2)\""));
+        assertEquals(result, logs);
+    }
+
+    @Test
     public void checkParseLogRequestResourceName() {
         assertEquals("product_1", parser.parseLogRequestResource(REQUEST_SAMPLE));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validUserAgentPart")
+    public void checkParseUserAgentMethod(String userAgent, String expected) {
+        assertEquals(expected, parser.parseUserAgent(userAgent));
     }
 }
